ce60dc53f3f8f0842de41f6cf9d42133491ef6fc
Lev Livshits <livshitz.lewa@yandex.ru>
2022-03-14 23:21:46 +0300

    merge

--- src/hw4/Tree.py
+++ src/hw4/Tree.py
@@ -11,9 +11,6 @@ class TreapNode:
 
 class Treap(TreapNode):
 
-    def __str__(self):
-        return str(f"Key: {self.key}, priority:{self.priority}")
-
     def print(tree: TreapNode):
         if tree is None:
             return
@@ -28,11 +25,24 @@ class Treap(TreapNode):
             node.right = current.right
             current.left
 
+    @staticmethod
+    def merge(first_tree: TreapNode, second_tree: TreapNode):
+        if first_tree is None:
+            return second_tree
+        if second_tree is None:
+            return first_tree
+        if first_tree.priority > second_tree.priority:
+            first_tree.right = Treap.merge(first_tree.right, second_tree)
+            return first_tree
+        else:
+            second_tree.left = Treap.merge(first_tree, second_tree.left)
+            return second_tree
+
+
 root = Treap(2, 2)
 left = TreapNode(1, 1)
 right = TreapNode(3, 3)
 right
 root.left = left
 root.right = right
-
 root.print()
