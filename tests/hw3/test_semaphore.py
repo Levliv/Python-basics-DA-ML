932bff341e69acd4079aaa73a9a480283e70cb6a
Lev Livshits <livshitz.lewa@yandex.ru>
2022-03-30 18:10:02 +0300

    changes

--- tests/hw3/test_semaphore.py
+++ tests/hw3/test_semaphore.py
@@ -1,19 +1,20 @@
 from threading import Thread
-from src.hw3.semaphore import *
+from src.hw3.semaphore import ConcurrentDict
 
 
-def create_string(dictionary, key, value):
+def create_string(dictionary: dict, key: str, value: int):
     with dictionary.modify_safely as dictionary:
         dictionary[key] = value
 
 
 def test_multi_threading():
     first_dict = ConcurrentDict()
-    threads = [Thread(target=create_string, args=(first_dict, f"key:{number}", number)) for number in range(30)]
+    threads = [Thread(target=create_string, args=(first_dict, f"key_{number}", number)) for number in range(1, 5)]
     for thread in threads:
-        thread.Start()
+        thread.start()
     for thread in threads:
-        thread.Join()
+        thread.join()
 
-    for number in range(30):
-        assert first_dict[f"key_{number}"] == number
+    with first_dict.modify_safely() as d:
+        for number in range(1, 5):
+            assert d[f"key_{number}"] == number
