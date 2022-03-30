33df634b773d889c7e13992bf899c8c86587c8b8
Lev Livshits <livshitz.lewa@yandex.ru>
2022-03-30 21:48:25 +0300

    fixed tests

--- tests/hw3/test_image_downloader.py
+++ tests/hw3/test_image_downloader.py
@@ -1,10 +1,12 @@
 import pytest
-from os import rmdir
+from os import rmdir, listdir, remove
 from src.hw3.image_downloader import downloader
 
 
-def skip():
+def test_simple():
     directory_name = downloader(4)
     print(directory_name)
+    assert len(listdir(directory_name)) == 4
+    for file in listdir(directory_name):
+        remove(f"{directory_name}/{file}")
     rmdir(directory_name)
-    assert 0 == 0
