d687a71c0fe0f7b3bdd96e4ee9aa937c90ae5be5
Lev Livshits <livshitz.lewa@yandex.ru>
2022-04-29 14:24:23 +0300

    style changes

--- tests/test2/test_task3.py
+++ tests/test2/test_task3.py
@@ -3,12 +3,23 @@ from src.test2.task3 import count_top_words, count_sentences
 
 
 def test_sentence():
-    assert count_sentences('tests/test2/Data/test.txt') == 12
+    assert count_sentences("tests/test2/Data/test.txt") == 12
 
 
 def test_simple_word_counter():
-    assert count_top_words('tests/test2/Data/test.txt', 1) == ['to']
+    assert count_top_words("tests/test2/Data/test.txt", 1) == ["to"]
 
 
 def test_pro_word_counter():
-    assert count_top_words('tests/test2/Data/test.txt', 10) == ['to', 'women', 'are', 's', 'in', 'so', 'of', 'their', 'children', 'that']
+    assert count_top_words("tests/test2/Data/test.txt", 10) == [
+        "to",
+        "women",
+        "are",
+        "s",
+        "in",
+        "so",
+        "of",
+        "their",
+        "children",
+        "that",
+    ]
