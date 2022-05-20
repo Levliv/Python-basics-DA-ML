81df3bd18759af08c708b3b6b1ae0efbed4f4f83
Levliv <50172336+Levliv@users.noreply.github.com>
2022-05-21 00:06:13 +0300

    Update test_task2.py

--- tests/test3/task2/test_task2.py
+++ tests/test3/task2/test_task2.py
@@ -15,5 +15,5 @@ def test_example():
                 html.p("Third string.")
     print(os.getcwd())
     print(os.path.exists("tests/test3/task2/expected.txt"))
-    assert filecmp.cmp("tests/test3/task2/expected.txt", "src/test3/task2/test.txt")
+    assert filecmp.cmp("tests/test3/task2/expected.txt", "test.txt")
     os.remove("src/test3/task2/test.txt")
