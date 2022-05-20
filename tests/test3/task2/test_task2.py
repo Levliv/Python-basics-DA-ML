130cc2815710dea15b5b123e7d873c73dd3e68ee
Levliv <50172336+Levliv@users.noreply.github.com>
2022-05-21 00:10:47 +0300

    Update test_task2.py

--- tests/test3/task2/test_task2.py
+++ tests/test3/task2/test_task2.py
@@ -1,8 +1,6 @@
 import pytest
 from src.test3.task2.html_manager import *
 import filecmp
-import os
-
 
 def test_example():
     html = HTML()
@@ -13,6 +11,7 @@ def test_example():
                 html.p("second string.")
             with html.div():
                 html.p("Third string.")
+     
     html.generate('test.txt')
     assert filecmp.cmp("tests/test3/task2/expected.txt", "src/test3/task2/test.txt")
     os.remove("src/test3/task2/test.txt")
