5ddef76378605fc85d9b89cc5651b8b7d59d4e7f
Lev Livshits <livshitz.lewa@yandex.ru>
2022-05-26 22:58:03 +0300

    file rename

--- tests/test3/task1/test_task1.py
+++
@@ -1,9 +0,0 @@
-import pytest
-import os
-from src.test3.task1.sprites import generator
-
-
-def test_file():
-    generator(8, 8, 7, 3, "test.png")
-    assert os.path.exists("test.png")
-    os.remove("test.png")
