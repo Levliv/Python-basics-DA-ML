ab0afbc61ae8611572d608023ce621193a89b49d
Lev Livshits <livshitz.lewa@yandex.ru>
2022-06-02 21:01:00 +0300

    working version

--- src/final_test/__main__.py
+++ src/final_test/__main__.py
@@ -11,24 +11,40 @@ bot = telebot.TeleBot("5532763302:AAH0Ovnsc0rT2F5EPbHZKd6qPS_-6FVePsg")
 def start_help(message):
     bot.reply_to(
         message,
-        "Привет, {0}!\nОтправьте фотографию, а я её раскрашу.\nЕсли нужна помощь, отправьте /help".format(
+        "Привет, {0}!\nОтправьте фотографию (.jpg), а я её раскрашу.\nЕсли нужна помощь, отправьте /help.\n"
+        "Чтобы получить результат преобразования, отправьте /results".format(
             message.from_user.first_name
         ),
     )
+
+
+@bot.message_handler(commands=["results"])
+def get_results(message):
+    user_id = message.from_user.id
+    dir_path = '/'.join(['photos', 'colored_' + str(user_id)])
+    user_colored_photos = os.listdir(dir_path)
+    for photo in user_colored_photos:
+        photo_path = '/'.join([dir_path, photo])
+        with open(photo_path, 'rb') as file:
+            bot.send_photo(user_id, file)
+        os.remove(photo_path)
+    bot.reply_to(message, 'После получения результата ваши фотографии будут удалены и храниться только у вас.')
 
 
 @bot.message_handler(content_types=["photo"])
 def handle_text(message):
     file_info = bot.get_file(message.photo[len(message.photo) - 1].file_id)
     downloaded_file = bot.download_file(file_info.file_path)
-    src = "{0}/{1}/{2}".format("photos", message.from_user.id, file_info.file_path.split("/")[-1])
-    dir_path = "/".join(src.split("/")[:-1])
+    save_file_path = "{0}/{1}/{2}".format("photos", message.from_user.id, file_info.file_path.split("/")[-1])
+    dir_path = "/".join(save_file_path.split("/")[:-1])
     if not os.path.exists(dir_path):
         os.mkdir(dir_path)
-    with open(src, "wb") as new_file:
+    with open(save_file_path, "wb") as new_file:
         new_file.write(downloaded_file)
-    bot.reply_to(message, "Фото добавлено")
-    image_colorize(src)
+
+    bot.reply_to(message, "Фото добавлено.\nЧтобы получить раскрашенную фотографию используйте /results")
+    image_colorize(save_file_path)
+    os.remove(save_file_path)
 
 
 @bot.message_handler(func=lambda m: True)
