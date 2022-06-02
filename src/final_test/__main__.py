e3e4547ec877121d5bbaa248e5bd4fb1d77d8d76
Lev Livshits <livshitz.lewa@yandex.ru>
2022-06-02 23:29:42 +0300

    final version

--- src/final_test/__main__.py
+++ src/final_test/__main__.py
@@ -2,9 +2,12 @@ import telebot
 import os
 from types import *
 from image_pocessing import image_colorize
+from file_handler import save_files
 
-
-bot = telebot.TeleBot("5532763302:AAH0Ovnsc0rT2F5EPbHZKd6qPS_-6FVePsg")
+secret_key = ""
+with open("secrets.txt") as file:
+    secret_key = file.readline()
+bot = telebot.TeleBot(secret_key)
 
 
 @bot.message_handler(commands=["start", "help"])
@@ -20,26 +23,23 @@ def start_help(message):
 def get_results(message):
     user_id = message.from_user.id
     dir_path = "/".join(["photos", "colored_" + str(user_id)])
-    user_colored_photos = os.listdir(dir_path)
-    for photo in user_colored_photos:
-        photo_path = "/".join([dir_path, photo])
-        with open(photo_path, "rb") as file:
-            bot.send_photo(user_id, file)
-        os.remove(photo_path)
-    bot.reply_to(message, "После получения результата ваши фотографии будут удалены и храниться только у вас.")
+    if not len(os.listdir(dir_path)):
+        bot.reply_to(message, "Вы уже скачали все фоторгафии \U000026C4.\nЗагружайте еще!")
+    else:
+        user_colored_photos = os.listdir(dir_path)
+        for photo in user_colored_photos:
+            photo_path = "/".join([dir_path, photo])
+            with open(photo_path, "rb") as photo_file:
+                bot.send_photo(user_id, photo_file)
+            os.remove(photo_path)
+        bot.reply_to(message, "После получения результата ваши фотографии будут удалены и сохранятся только у вас.")
 
 
 @bot.message_handler(content_types=["photo"])
 def handle_text(message):
     file_info = bot.get_file(message.photo[len(message.photo) - 1].file_id)
     downloaded_file = bot.download_file(file_info.file_path)
-    save_file_path = "{0}/{1}/{2}".format("photos", message.from_user.id, file_info.file_path.split("/")[-1])
-    dir_path = "/".join(save_file_path.split("/")[:-1])
-    if not os.path.exists(dir_path):
-        os.mkdir(dir_path)
-    with open(save_file_path, "wb") as new_file:
-        new_file.write(downloaded_file)
-
+    save_file_path = save_files(message, file_info, downloaded_file)
     bot.reply_to(message, "Фото добавлено.\nЧтобы получить раскрашенную фотографию используйте /results")
     image_colorize(save_file_path)
     os.remove(save_file_path)
